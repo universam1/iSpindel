16fc0ece0061a371ebbc942c68067567e3a8bf7d
Lang, Samuel <slang@deu.wtbts.net>
2017-01-05 10:35:38 +0100

    refactor

---
+++ pics/PCB/pfade.ps1
@@ -0,0 +1,3 @@
+﻿Get-ChildItem -Path "C:\Users\slang\Dropbox\Arduino\_Code\iSpindel\pics\PCB" | Rename-Item -NewName {$_.Name.Replace("--","-")}
+
+Get-ChildItem -Path "C:\Users\slang\Dropbox\Arduino\_Code\iSpindel\pics\PCB" | % {"![{0}](../pics/PCB/{0})" -f $_.Name}
