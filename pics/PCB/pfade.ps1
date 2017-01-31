df4e94c60b7354c806951421225a69c4caa903ef
Lang <SLang@bethel.jw.org>
2017-01-31 16:13:37 +0100

    fix MD

--- pics/PCB/pfade.ps1
+++ pics/PCB/pfade.ps1
@@ -1,4 +1,5 @@
-﻿Get-ChildItem -Path "C:\Users\slang\Dropbox\Arduino\_Code\iSpindel\pics\PCB" | Rename-Item -NewName {$_.Name.Replace("--","-").Replace(" ","").Replace("ü","ue").Replace("ä","ae").Replace("ö","oe")}
+﻿Get-ChildItem -Path "C:\Users\slang\Dropbox\Arduino\_Code\iSpindel\pics\PCB" | 
+Rename-Item -NewName {$_.Name.Replace("--","-").Replace(" ","").Replace("ü","ue").Replace("ä","ae").Replace("ö","oe").Replace("Ü","Ue").Replace("Ä","Ae").Replace("Ö","Oe").Replace("ß","ss")}
 Get-ChildItem -Path "C:\Users\slang\Dropbox\Arduino\_Code\iSpindel\pics\PCB" | Rename-Item -NewName  {$_.Name -replace "^([0-9]{1})-",'0$1-'}
 
-Get-ChildItem -Path "C:\Users\slang\Dropbox\Arduino\_Code\iSpindel\pics\PCB" | % {"![{0}](../pics/PCB/{0})" -f $_.Name} | clip
+Get-ChildItem -Path "C:\Users\slang\Dropbox\Arduino\_Code\iSpindel\pics\PCB" | % {"{1} ![{0}](../pics/PCB/{0})" -f $_.Name, $_.BaseName } | clip
