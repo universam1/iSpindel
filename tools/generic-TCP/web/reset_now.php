9e1b619b987950f3df74fb25b1ec8ef4906e2221
bierkiki <markuseckstein@gmx.de>
2017-04-21 18:01:57 +0200

    Update reset_now.php

--- tools/generic-TCP/web/reset_now.php
+++ tools/generic-TCP/web/reset_now.php
@@ -8,7 +8,7 @@ if(!isset($_GET['name'])) $_GET['name'] = 'iSpindel000'; else $_GET['name'] = $_
 
 $Name = $_GET['name'];
 
-$q_sql = mysql_query( "INSERT INTO data (Timestamp, Name, resetFlag)
+$q_sql = mysql_query( "INSERT INTO Data (Timestamp, Name, resetFlag)
                       VALUES (NOW(),'$Name', true)")
                        or die(mysql_error());
                        
