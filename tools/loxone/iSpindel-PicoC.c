151356f9b6f8a49bcaa5d39307d570ef02d7aef4
joergp <joerg.pressel@three-2-one.com>
2017-04-23 00:50:50 +0200

    fixed loxone memory leak, added app screenshot

--- tools/loxone/iSpindel-PicoC.c
+++ tools/loxone/iSpindel-PicoC.c
@@ -1,46 +1,50 @@
-// TQ1 > Name der Spindel
-// AQ1 > Tilt
-// AQ2 > Plato
-// AQ3 > Temperatur
-// AQ4 > Battery
-
 int nEvents;
 int idx;
-char* Text;
 char strTemp[30];
 float out;
 
+enum OutputPorts
+{
+    tilt,            // AQ1
+    plato,           // AQ2
+	temperature,	 // AQ3
+	battery			 // AQ4
+};
+
 while(TRUE)
-    {
-    nEvents = 1;
+{
+	char* Text;
+	nEvents = getinputevent();
     if (nEvents & 0x01)
         {
         Text = getinputtext(0);
 		idx = strfind(Text, " ", 0);
-		if (idx < 0) {return;}
+		if (idx < 0) {free(Text); return;}
 		strncpy(strTemp,Text,idx);
 		//name
 		setoutputtext(0, strTemp);
 		Text = strstrskip(Text, " ");
 		idx = strfind(Text, " ", 0);
-		if (idx < 0) {return;}
+		if (idx < 0) {free(Text); return;}
 		//tilt
 		strncpy(strTemp,Text,idx);
-		out = batoi(strTemp);
-		setoutput(0, out);
+		out = batof(strTemp);
+		setoutput(tilt, out);
 		out = 0.0045*out*out-0.123*out+1.0689;
-		setoutput(1, out);
+		//plato
+		setoutput(plato, out);
 		Text = strstrskip(Text, " ");
 		idx = strfind(Text, " ", 0);
-		if (idx < 0) {return;}
+		if (idx < 0) {free(Text); return;}
 		//temperature
 		strncpy(strTemp,Text,idx);
-		out = batoi(strTemp);
-		setoutput(2, out);
+		out = batof(strTemp);
+		setoutput(temperature, out);
 		Text = strstrskip(Text, " ");
 		//battery
-		out = batoi(Text);
-		setoutput(3, out);
+		out = batof(Text);
+		setoutput(battery, out);
+		free(Text);
         }
-    sleep(100);
-    }
+    sleep(1000);
+}
