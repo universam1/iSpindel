cbe71bec45abb36d1b27dc387bd1c613f18e1f1d
mestrode <henning.hanssen@mestro.de>
2018-03-15 01:19:56 +0100

    urlencode

--- pio/src/iSpindel.cpp
+++ pio/src/iSpindel.cpp
@@ -287,7 +287,6 @@ String urlencode(String str)
   char c;
   char code0;
   char code1;
-  char code2;
   for (int i = 0; i < str.length(); i++)
   {
     c = str.charAt(i);
@@ -301,22 +300,22 @@ String urlencode(String str)
     }
     else
     {
+      //convert lower 4 bits into hex value
       code1 = (c & 0xf) + '0';
       if ((c & 0xf) > 9)
       {
         code1 = (c & 0xf) - 10 + 'A';
       }
+      //work on higher 4 bits
       c = (c >> 4) & 0xf;
       code0 = c + '0';
       if (c > 9)
       {
         code0 = c - 10 + 'A';
       }
-      code2 = '\0';
       encodedString += '%';
       encodedString += code0;
       encodedString += code1;
-      //encodedString+=code2;
     }
     yield();
   }
