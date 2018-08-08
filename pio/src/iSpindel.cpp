34c62d2cf2285d929b7ae232cde6a395a49afd05
jmelhus <jmelhus@gmail.com>
2018-08-08 22:11:23 +0200

    Update iSpindel.cpp

--- pio/src/iSpindel.cpp
+++ pio/src/iSpindel.cpp
@@ -6,6 +6,7 @@ All rights reserverd by S.Lang <universam@web.de>
 **************************************************************/
 
 // includes go here
+#include <PubSubClient.h>
 #include "Globals.h"
 #include "MPUOffset.h"
 // #endif
@@ -77,6 +78,8 @@ String my_psk;
 uint8_t my_api;
 uint32_t my_sleeptime = 15 * 60;
 uint16_t my_port = 80;
+char my_user[TKIDSIZE];
+char my_pw[TKIDSIZE];
 float my_vfact = ADCDIVISOR;
 int16_t my_aX = UNINIT, my_aY = UNINIT, my_aZ = UNINIT;
 uint8_t my_tempscale = TEMP_CELSIUS;
@@ -159,6 +162,10 @@ bool readConfig()
             my_api = json["API"];
           if (json.containsKey("Port"))
             my_port = json["Port"];
+          if (json.containsKey("User"))
+            strcpy(my_user, json["User"]);
+          if (json.containsKey("PW"))
+            strcpy(my_pw, json["PW"]);
           if (json.containsKey("URL"))
             strcpy(my_url, json["URL"]);
           if (json.containsKey("DB"))
@@ -166,7 +173,7 @@ bool readConfig()
           if (json.containsKey("Job"))
             strcpy(my_job, json["Job"]);
           if (json.containsKey("Instance"))
-            strcpy(my_instance, json["Instance"]);            
+            strcpy(my_instance, json["Instance"]);
           if (json.containsKey("Vfact"))
             my_vfact = json["Vfact"];
           if (json.containsKey("TS"))
@@ -352,29 +359,33 @@ bool startConfiguration()
 
   WiFiManagerParameter api_list(HTTP_API_LIST);
   WiFiManagerParameter custom_api("selAPI", "selAPI", String(my_api).c_str(),
-                                  20, TYPE_HIDDEN, WFM_NO_LABEL);
+    20, TYPE_HIDDEN, WFM_NO_LABEL);
 
   WiFiManagerParameter custom_name("name", "iSpindel Name", htmlencode(my_name).c_str(),
-                                   TKIDSIZE*2);
+    TKIDSIZE * 2);
   WiFiManagerParameter custom_sleep("sleep", "Update Intervall (s)",
-                                    String(my_sleeptime).c_str(), 6, TYPE_NUMBER);
+    String(my_sleeptime).c_str(), 6, TYPE_NUMBER);
   WiFiManagerParameter custom_token("token", "Token", htmlencode(my_token).c_str(),
-                                    TKIDSIZE*2);
+    TKIDSIZE * 2);
   WiFiManagerParameter custom_server("server", "Server Address",
-                                     my_server, TKIDSIZE);
+    my_server, TKIDSIZE);
   WiFiManagerParameter custom_port("port", "Server Port",
-                                   String(my_port).c_str(), TKIDSIZE,
-                                   TYPE_NUMBER);
+    String(my_port).c_str(), TKIDSIZE,
+    TYPE_NUMBER);
+  WiFiManagerParameter custom_user("user", "Username", htmlencode(my_user).c_str(),
+    TKIDSIZE * 2);
+  WiFiManagerParameter custom_pw("pw", "Password", htmlencode(my_pw).c_str(),
+    TKIDSIZE * 2);
   WiFiManagerParameter custom_url("url", "Server URL", my_url, TKIDSIZE);
   WiFiManagerParameter custom_db("db", "InfluxDB db", my_db, TKIDSIZE);
   WiFiManagerParameter custom_job("job", "Prometheus job", my_job, TKIDSIZE);
   WiFiManagerParameter custom_instance("instance", "Prometheus instance", my_instance, TKIDSIZE);
   WiFiManagerParameter custom_vfact("vfact", "Battery conversion factor",
-                                    String(my_vfact).c_str(), 7, TYPE_NUMBER);
+    String(my_vfact).c_str(), 7, TYPE_NUMBER);
   WiFiManagerParameter tempscale_list(HTTP_TEMPSCALE_LIST);
   WiFiManagerParameter custom_tempscale("tempscale", "tempscale",
-                                        String(my_tempscale).c_str(),
-                                        5, TYPE_HIDDEN, WFM_NO_LABEL);
+    String(my_tempscale).c_str(),
+    5, TYPE_HIDDEN, WFM_NO_LABEL);
 
   wifiManager.addParameter(&custom_name);
   wifiManager.addParameter(&custom_sleep);
@@ -393,13 +404,15 @@ bool startConfiguration()
   wifiManager.addParameter(&custom_token);
   wifiManager.addParameter(&custom_server);
   wifiManager.addParameter(&custom_port);
+  wifiManager.addParameter(&custom_user);
+  wifiManager.addParameter(&custom_pw);
   wifiManager.addParameter(&custom_url);
   wifiManager.addParameter(&custom_db);
   wifiManager.addParameter(&custom_job);
   wifiManager.addParameter(&custom_instance);
   WiFiManagerParameter custom_polynom_lbl("<hr><label for=\"POLYN\">Gravity conversion<br/>ex. \"-0.00031*tilt^2+0.557*tilt-14.054\"</label>");
   wifiManager.addParameter(&custom_polynom_lbl);
-  WiFiManagerParameter custom_polynom("POLYN", "Polynominal", htmlencode(my_polynominal).c_str(), 70*2, WFM_NO_LABEL);
+  WiFiManagerParameter custom_polynom("POLYN", "Polynominal", htmlencode(my_polynominal).c_str(), 70 * 2, WFM_NO_LABEL);
   wifiManager.addParameter(&custom_polynom);
 
   wifiManager.setConfSSID(htmlencode(my_ssid));
@@ -420,6 +433,8 @@ bool startConfiguration()
 
   my_api = String(custom_api.getValue()).toInt();
   my_port = String(custom_port.getValue()).toInt();
+  validateInput(custom_user.getValue(), my_user);
+  validateInput(custom_pw.getValue(), my_pw);
   my_tempscale = String(custom_tempscale.getValue()).toInt();
   validateInput(custom_url.getValue(), my_url);
 
@@ -456,7 +471,7 @@ bool saveConfig()
 
   // if SPIFFS is not usable
   if (!SPIFFS.begin() || !SPIFFS.exists(CFGFILE) ||
-      !SPIFFS.open(CFGFILE, "w"))
+    !SPIFFS.open(CFGFILE, "w"))
     formatSpiffs();
 
   DynamicJsonBuffer jsonBuffer;
@@ -470,6 +485,8 @@ bool saveConfig()
   json["Server"] = my_server;
   json["API"] = my_api;
   json["Port"] = my_port;
+  json["User"] = my_user;
+  json["PW"] = my_pw;
   json["URL"] = my_url;
   json["DB"] = my_db;
   json["Job"] = my_job;
@@ -521,6 +538,20 @@ bool uploadData(uint8_t service)
     sender.add("RSSI", WiFi.RSSI());
     CONSOLELN(F("\ncalling Ubidots"));
     return sender.sendUbidots(my_token, my_name);
+  }
+#endif
+
+#ifdef API_MQTT
+  if (service == DTMQTT)
+  {
+    sender.add("tilt", Tilt);
+    sender.add("temperature", scaleTemperature(Temperatur));
+    sender.add("battery", Volt);
+    sender.add("gravity", Gravity);
+    sender.add("interval", my_sleeptime);
+    sender.add("RSSI", WiFi.RSSI());
+    CONSOLELN(F("\ncalling MQTT"));
+    return sender.sendMQTT(my_server, my_port, my_user, my_pw, my_name);
   }
 #endif
 
@@ -773,7 +804,7 @@ float getTemperature(bool block = false)
     DSrequested = false;
 
     if (t == DEVICE_DISCONNECTED_C || // DISCONNECTED
-        t == 85.0)                    // we read 85 uninitialized
+      t == 85.0)                    // we read 85 uninitialized
     {
       CONSOLELN(F("ERROR: OW DISCONNECTED"));
       pinMode(ONE_WIRE_BUS, OUTPUT);
@@ -804,7 +835,7 @@ float calculateGravity()
   double _temp = Temperatur;
   float _gravity = 0;
   int err;
-  te_variable vars[] = {{"tilt", &_tilt}, {"temp", &_temp}};
+  te_variable vars[] = { { "tilt", &_tilt }, { "temp", &_temp } };
   te_expr *expr = te_compile(my_polynominal, vars, 2, &err);
 
   if (expr)
@@ -851,7 +882,6 @@ bool connectBackupCredentials()
 
 void setup()
 {
-
   Serial.begin(115200);
 
   CONSOLELN(F("\nFW " FIRMWAREVERSION));
@@ -891,7 +921,7 @@ void setup()
     {
       // test if ssid exists
       if (WiFi.SSID() == "" &&
-          my_ssid != "" && my_psk != "")
+        my_ssid != "" && my_psk != "")
       {
         connectBackupCredentials();
       }
@@ -939,11 +969,11 @@ void setup()
     accelgyro.dmpGetEuler(euler, &q);
 
     /*
-      for (int i = 1; i < 64; i++) {
-        CONSOLE(fifoBuffer[i]);
-        CONSOLE(" ");
-      }
-   */
+    for (int i = 1; i < 64; i++) {
+    CONSOLE(fifoBuffer[i]);
+    CONSOLE(" ");
+    }
+    */
 
     CONSOLE(F("euler\t"));
     CONSOLE((euler[0] * 180 / M_PI));
