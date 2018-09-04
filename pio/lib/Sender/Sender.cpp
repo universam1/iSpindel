7cd89c469af5d259dd5d1a7a64ca529909c0743e
Fernando Garcia <fefegarcia_1@hotmail.com>
2018-09-04 17:03:03 -0300

    Add messages to return code and limit connection attempt (#202)
    * Add messages to return code and limit connection attempt
    
    It will prevent waste all the battery when the broker is down and add some readable messages for debug purpose.
    
    * Display temperature in info page according to selected unit.

--- pio/lib/Sender/Sender.cpp
+++ pio/lib/Sender/Sender.cpp
@@ -39,7 +39,9 @@ bool SenderClass::sendMQTT(String server, uint16_t port, String username, String
     _mqttClient.setServer(server.c_str(), port);
     _mqttClient.setCallback([this](char *topic, byte *payload, unsigned int length) { this->mqttCallback(topic, payload, length); });
 
-    while (!_mqttClient.connected())
+    byte i = 0;
+
+    while (!_mqttClient.connected() && (i < 3))
     {
         CONSOLELN(F("Attempting MQTT connection"));
         // Attempt to connect
@@ -49,10 +51,51 @@ bool SenderClass::sendMQTT(String server, uint16_t port, String username, String
         }
         else
         {
-            CONSOLELN(F("Failed MQTT connection, return code:"));
-            CONSOLELN(_mqttClient.state());
+            CONSOLE(F("Failed MQTT connection, return code:"));
+
+            int Status = _mqttClient.state();
+
+            switch (Status)
+            {
+            case -4:
+              CONSOLELN(F("Connection timeout"));
+              break;
+
+            case -3:
+              CONSOLELN(F("Connection lost"));
+              break;
+
+            case -2:
+              CONSOLELN(F("Connect failed"));
+              break;
+
+            case -1:
+              CONSOLELN(F("Disconnected"));
+              break;
+
+            case 1:
+              CONSOLELN(F("Bad protocol"));
+              break;
+
+            case 2:
+              CONSOLELN(F("Bad client ID"));
+              break;
+
+            case 3:
+              CONSOLELN(F("Unavailable"));
+              break;
+
+            case 4:
+              CONSOLELN(F("Bad credentials"));
+              break;
+
+            case 5:
+              CONSOLELN(F("Unauthorized"));
+              break;
+            }
             CONSOLELN(F("Retrying MQTT connection in 5 seconds"));
             // Wait 5 seconds before retrying
+            i++;
             delay(5000);
         }
     }
